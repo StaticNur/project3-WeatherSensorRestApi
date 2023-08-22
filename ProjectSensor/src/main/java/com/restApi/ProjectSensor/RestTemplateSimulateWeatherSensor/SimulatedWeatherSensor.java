9c12b567c6830847d838564d4745bcb6a8733e4b
Mukhammadnur <gsjxne@gmail.com>
2023-08-22 13:22:21 +0300

    add building a graph

--- ProjectSensor/src/main/java/com/restApi/ProjectSensor/RestTemplateSimulateWeatherSensor/SimulatedWeatherSensor.java
+++ ProjectSensor/src/main/java/com/restApi/ProjectSensor/RestTemplateSimulateWeatherSensor/SimulatedWeatherSensor.java
@@ -2,12 +2,16 @@ package com.restApi.ProjectSensor.RestTemplateSimulateWeatherSensor;
 
 import com.restApi.ProjectSensor.dto.MeasurementDTO;
 import com.restApi.ProjectSensor.dto.SensorDTO;
+import org.knowm.xchart.style.lines.SeriesLines;
+import org.knowm.xchart.style.markers.SeriesMarkers;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
-
+import org.knowm.xchart.*;
+import org.knowm.xchart.style.Styler;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class SimulatedWeatherSensor {
@@ -48,13 +52,24 @@ public class SimulatedWeatherSensor {
         httpHeaders.setContentType(MediaType.APPLICATION_JSON);
 
         MeasurementDTO[] response = restTemplate.getForObject(url,MeasurementDTO[].class);
-        for (MeasurementDTO measurementDTO: response){
+        /*for (MeasurementDTO measurementDTO: response){
             System.out.println(measurementDTO.toString());
-        }
+        }*/
+        buildingAGraph(response);
     }
-    public static void buildingAGraph(){
-        //ExampleChart<XYChart> exampleChart = new AreaChart01();
-        //XYChart chart = exampleChart.getChart();
-        //new SwingWrapper<XYChart>(chart).displayChart();
+    public static void buildingAGraph(MeasurementDTO[] response){
+        List<Double> xData = new ArrayList<>();
+        List<Double> yData = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            xData.add((double) i);
+            yData.add(response[i].getValue());
+        }
+        XYChart chart = new XYChartBuilder().width(800).height(600).title("temperature graph").xAxisTitle("Measurement Index").yAxisTitle("temperature").build();
+        XYSeries series = chart.addSeries("temperature", xData, yData);
+        series.setMarker(SeriesMarkers.NONE);
+        series.setLineStyle(SeriesLines.SOLID);
+        chart.getStyler().setLegendPosition(Styler.LegendPosition.InsideNW);
+
+        new SwingWrapper<>(chart).displayChart();
     }
 }
